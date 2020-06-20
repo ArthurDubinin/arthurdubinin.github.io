273297564fe70873caa50e1ee23411b309d862ea
Arthur Dubinin <HelloArthurDubinin@gmail.com>
2020-06-20 16:04:41 +0300

    Do not works(

--- js/main.js
+++ js/main.js
@@ -2817,11 +2817,3 @@ jQuery(function($){
 		});
 	}());
 });
-$(document).ready(function(){
-$("#menu").on("click","a", function (event) {
-event.preventDefault();
-var id  = $(this).attr('href'),
-top = $(id).offset().top;
-$('body,html').animate({scrollTop: top}, 1500);
-});
-});
